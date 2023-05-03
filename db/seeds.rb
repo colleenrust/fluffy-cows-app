fc9e617c618eddf072d76c9b2855f5fed3982a6d
Colleen <colleenrust524@EMAIL.com>
2023-05-03 15:41:42 -0500

    resized the buttons to fit the whole screen and added the cows to seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cows = Cow.create([{photo:"https://pbs.twimg.com/media/E3c9SNdXMAQye92.jpg:large", compliment: "You're doing amazing, sweetie!"}, {photo:   "https://i.pinimg.com/originals/78/ac/4a/78ac4ac4187e998abcd1d8d23db171e8.jpg", compliment:"You're that “Nothing” when people ask me what I'm thinking about."}, {photo:"https://kitchenfunwithmy3sons.com/wp-content/uploads/2020/02/fluffy-cow-1-scaled.jpg", compliment:"You are more fun than anyone or anything I know, including bubble wrap."}, {photo:"https://e0.pxfuel.com/wallpapers/462/249/desktop-wallpaper-pet-cows-fluffy-cows-cute-baby-baby-calf.jpg", compliment:"Is that your picture next to “charming” in the dictionary?"}, {photo: "https://static.boredpanda.com/blog/wp-content/uploads/2017/10/cute-baby-highland-cattle-calves-fb8-png__700.jpg", compliment:"You shine like the Chrysler Building!"}])
